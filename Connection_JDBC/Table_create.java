b99da2ba5e1cd414a03c8d3f94b55dda80d35357
Saurabh Kumar <54509629+Saurabh2509@users.noreply.github.com>
2021-12-02 00:50:00 +0530

    Create Table_create.java

---
+++ Connection_JDBC/Table_create.java
@@ -0,0 +1,39 @@
+/*Author ~ Saurabh Kumar
+@Copyright 2021*/
+ 
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC1 {
+	public static void main(String[] args) throws SQLException {
+		Connection con =null;
+		String url ="jdbc:oracle:thin:@localhost:1521:xe";
+		PreparedStatement pst =null;
+		ResultSet res=null;
+		
+		try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		con =DriverManager.getConnection(url,"username","password");
+		 System.out.println("Connection stablished.");
+		
+		String sql="Create Table Student "+"(studentId int Not null Primary key ,"+ "StudentName varchar(10),"+"Branch varchar(20), "+"Percentage double precision )";
+		
+		 pst=con.prepareStatement(sql);
+	     res =pst.executeQuery();
+		System.out.println("Table Student Created");
+		
+		}
+		catch(ClassNotFoundException e) {
+		 System.out.println("No driver found");	
+		}
+
+	}
+
+}
